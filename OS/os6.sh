4ff86ae31cd9653cf751d92702b01be3e1276a15
Shrawan Saproo <102973966+avsk3234@users.noreply.github.com>
2022-12-29 23:34:37 +0530

    Add files via upload

---
+++ OS/os6.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#Variable Declaration
+num1=15
+num2=5
+
+#Arithmetic operations
+sum=$((num1+num2))
+diff=$((num1-num2))
+divide=$((num1/num2))
+product=$((num1*num2))
+
+#Displaying results
+echo -e "Sum: $sum\nDifference: $diff\nDivision: $divide\nMultiplication: $product\n"
---
+++ OS/os7.sh
@@ -0,0 +1,23 @@
+#Folder/Directory Creation
+mkdir test
+
+#Enter the directory named test
+cd test
+
+#Create four files
+touch file1 file2 file3 file4
+
+#Edit the files with nano to enter desired data
+nano file1
+nano file2
+nano file3
+nano file4
+
+#Searching for a particular name with grep command
+grep Aryan *
+
+#Search with -v flag
+grep -v Aryan *
+
+#Search with -i flag
+grep -i aryan *
